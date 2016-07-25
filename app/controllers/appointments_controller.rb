afcf671e1ada242bff436b0c5199bf6b5f7e2d02
Jeremy Hennig <jeremy.hennig@ualberta.ca>
2016-07-25 16:25:19 -0600

    other updates to appoinments form

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,13 +3,43 @@ class AppointmentsController < ApplicationController
   def index
   end
 
+  def show
+    puts YAML::dump(flash)
+
+    raise @flash.inspect
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      flash[:success] = 'Appointment has been saved'
+      redirect_to appointment_url(@appointment)   # short for redirect_to appointment_url(@appointment) see chapter 7.4.1 in railstutorial.org
+    else
+      render 'appointments/book'
+    end
+
+  end
+
   def new
   	@appointment = Appointment.new
-    @subject = [['Agricultural, Life and Environmental Sciences'],['Arts'],['Augustana Faculty Library'],['Business, School of'],['Campus Saint-Jean'],['Education'],\
-            ['Engineering'],['Law'],['Library and Information Studies, School of'],['Medicine and Dentistry'],['Native Studies'],['Nursing'], ['Pharmacy and Pharmaceutical Sciences'],\
-            ['Physical Education and Recreation'], ['Rehabilitation Medicine'], ['School of Public Health'], ['Science'], ['Interdisciplinary Collections and Subjects']]
+=begin
+        @subject = [['Agricultural, Life and Environmental Sciences'],['Arts'],['Augustana Faculty Library'],['Business, School of'],['Campus Saint-Jean'],['Education'],\
+        ['Engineering'],['Law'],['Library and Information Studies, School of'],['Medicine and Dentistry'],['Native Studies'],['Nursing'], ['Pharmacy and Pharmaceutical Sciences'],\
+        ['Physical Education and Recreation'], ['Rehabilitation Medicine'], ['School of Public Health'], ['Science'], ['Interdisciplinary Collections and Subjects']]
+=end
+
+    @subject = AppointmentSubject.order(:subject).take
+
+    abort @subject.inspect
 
    render 'appointments/book'
+  end
+
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:name, :email,  :one_card,  :best_times,   :topic, :subject, :sent_to, :assigned, :department )
   end
 
 end
